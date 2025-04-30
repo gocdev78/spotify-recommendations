6996a811d373c9d341a62918376dcc5f4c102fe0
huchlatymon <huchlatymon12@gmail.com>
2025-04-30 13:04:51 +0200

    adding infra management

--- infra/startup.sh
+++ infra/startup.sh
@@ -13,8 +13,8 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo apt-get update -y
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 echo "yes" > /tmp/STARTUP_FINISHED
