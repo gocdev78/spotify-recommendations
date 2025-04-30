c172076237f53dd363d28fa8cbb83587f4f34b9c
huchlatymon <huchlatymon12@gmail.com>
2025-04-30 13:22:07 +0200

    adding infra management

--- infra/startup.sh
+++ infra/startup.sh
@@ -16,5 +16,6 @@ echo \
 sudo apt-get update -y
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo usermod -aG docker $USER
 
 echo "yes" > /tmp/STARTUP_FINISHED
